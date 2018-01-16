7a496e53d5a8f7856530c4c9baaeee77561c2841
iamsachinrajput <iamsachinrajput@gmail.com>
2018-01-16 15:49:01 +0530

    removed double emails for stopping

--- miner.sh
+++ miner.sh
@@ -24,7 +24,7 @@ SKMIN=`ps -eaf | grep -i minergate-cli | grep -iv grep |wc -l`
                                 #(echo "$HOSTPUBIP will be rebooted \n logs are below \n";cat /skminer/log/miner_log_bad.txt /skminer/log/miner_log_good.txt /skminer/log/miner_log_check.txt ) |sendmail -v iamsachinrajput@gmail.com
                                 (echo "SUBJECT:mail 1 Miner will be stopped in $HOSTPUBIP due to low speed = $HRATE";echo "FROM:iamsachinrajput@gmail.com";echo "$HOSTPUBIP will be rebooted \n logs are below \n";echo " ========================= ";tail -5 /skminer/log/miner_log_check.txt ;echo "======================= \n"; tail -10 /skminer/log/miner_log_good.txt;echo "================== \n" ; tail -10 /skminer/log/miner_log_bad.txt;echo "==================== \n\n" ;cat /skminer/log/miner_log_bad.txt /skminer/log/miner_log_good.txt /skminer/log/miner_log_check.txt ) |sendmail -v iamsachinrajput@gmail.com
 
-(echo "SUBJECT: mail2 Miner will be stopped in $HOSTPUBIP due to low speed = $HRATE" ; cat /skminer/log/miner_log_bad.txt /skminer/log/miner_log_good.txt /skminer/log/miner_log_check.txt ) |sendmail iamsachinrajput@gmail.com
+#(echo "SUBJECT: mail2 Miner will be stopped in $HOSTPUBIP due to low speed = $HRATE" ; cat /skminer/log/miner_log_bad.txt /skminer/log/miner_log_good.txt /skminer/log/miner_log_check.txt ) |sendmail iamsachinrajput@gmail.com
 
 sleep 60
 init 0
