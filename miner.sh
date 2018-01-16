a9223685a5c114db79463abe375207cef518ad65
root <root@ip-172-31-24-86.ap-south-1.compute.internal>
2018-01-16 06:41:30 +0000

    add miner.sh

---
+++ miner.sh
@@ -0,0 +1,32 @@
+while [ 1 -eq 1 ]
+do
+
+SKMIN=`ps -eaf | grep -i minergate-cli | grep -iv grep |wc -l`
+        if [ $SKMIN -lt 1 ]
+        then
+                echo "`date` ====== miner is not running so starting up " >> /skminer/log/miner_log_check.txt
+                minergate-cli -user iamsachinrajput@gmail.com -xmr 4 2>> /skminer/log/miner_log_check.txt >> /skminer/log/miner_log_check.txt &
+                #minergate-cli -user iamsachinrajput@gmail.com -dsh 4 &
+                sleep 60
+                HRATE=`tail /skminer/log/miner_log_check.txt | grep -i H/s | tail -1 | awk '{print int($(NF-1))}'`
+                echo " $HOSTPUBIP is started and started at rate of $HRATE " |sendmail iamsachinrajput@gmail.com
+        else
+                sleep 60
+                HRATE=`tail /skminer/log/miner_log_check.txt | grep -i H/s | tail -1 | awk '{print int($(NF-1))}'`
+
+                        if [ $HRATE -gt 2 ]
+                        then
+                                echo " `date` ===== running good at rate of $HRATE " >> /skminer/log/miner_log_good.txt
+
+                        else
+                                echo " `date` ==== not good speed at rate of $HRATE ; so we will reboot `hostname` " >> /skminer/log/miner_log_bad.txt
+                                #(echo "$HOSTPUBIP will be rebooted \n logs are below \n";cat /skminer/log/miner_log_bad.txt /skminer/log/miner_log_good.txt /skminer/log/miner_log_check.txt ) |sendmail -v iamsachinrajput@gmail.com
+                                (echo "$HOSTPUBIP will be rebooted \n logs are below \n";echo " ========================= ";tail -5 /skminer/log/miner_log_check.txt ;echo "======================= \n"; tail -10 /skminer/log/miner_log_good.txt;echo "================== \n" ; tail -10 /skminer/log/miner_log_bad.txt;echo "==================== \n\n" ;cat /skminer/log/miner_log_bad.txt /skminer/log/miner_log_good.txt /skminer/log/miner_log_check.txt ) |sendmail -v iamsachinrajput@gmail.com
+
+sleep 20
+#init 0
+                                exit
+                        fi
+
+        fi
+done
