7d018f869008dfb96d384c440f91e31c388aa6ac
iamsachinrajput <iamsachinrajput@gmail.com>
2018-01-16 19:19:25 +0530

    update email msg subject

--- miner.sh
+++ miner.sh
@@ -10,7 +10,7 @@ SKMIN=`ps -eaf | grep -i minergate-cli | grep -iv grep |wc -l`
                 #minergate-cli -user iamsachinrajput@gmail.com -dsh 4 &
                 sleep 60
                 HRATE=`tail /skminer/log/miner_log_check.txt | grep -i H/s | tail -1 | awk '{print int($(NF-1))}'`
-                echo "SUBJECT:SKMINER-SKRAJ $HOSTPUBIP started at $HRATE /s " |sendmail iamsachinrajput@gmail.com
+                echo "SUBJECT:$HRATE SKMINER-SKRAJ $HOSTPUBIP started at $HRATE /s " |sendmail iamsachinrajput@gmail.com
         else
                 sleep 90
                 HRATE=`tail /skminer/log/miner_log_check.txt | grep -i H/s | tail -1 | awk '{print int($(NF-1))}'`
@@ -22,7 +22,7 @@ SKMIN=`ps -eaf | grep -i minergate-cli | grep -iv grep |wc -l`
                         else
                                 echo " `date` ===== not good speed at rate of $HRATE ; so we will reboot `hostname` " >> /skminer/log/miner_log_bad.txt
                                 #(echo "$HOSTPUBIP will be rebooted \n logs are below \n";cat /skminer/log/miner_log_bad.txt /skminer/log/miner_log_good.txt /skminer/log/miner_log_check.txt ) |sendmail -v iamsachinrajput@gmail.com
-                                (echo "SUBJECT:SKMINER-SKRAJ stop $HOSTPUBIP due to $HRATE /s " ; echo "BODY:$HOSTPUBIP will be rebooted \n logs are below \n";echo " ========================= ";tail -5 /skminer/log/miner_log_check.txt ;echo "======================= \n"; tail -10 /skminer/log/miner_log_good.txt;echo "================== \n" ; tail -10 /skminer/log/miner_log_bad.txt;echo "==================== \n\n" ;cat /skminer/log/miner_log_bad.txt /skminer/log/miner_log_good.txt /skminer/log/miner_log_check.txt ) |sendmail -v iamsachinrajput@gmail.com
+                                (echo "SUBJECT:$HRATE SKMINER-SKRAJ stop $HOSTPUBIP due to $HRATE /s " ; echo "BODY:$HOSTPUBIP will be rebooted \n logs are below \n";echo " ========================= ";tail -5 /skminer/log/miner_log_check.txt ;echo "======================= \n"; tail -10 /skminer/log/miner_log_good.txt;echo "================== \n" ; tail -10 /skminer/log/miner_log_bad.txt;echo "==================== \n\n" ;cat /skminer/log/miner_log_bad.txt /skminer/log/miner_log_good.txt /skminer/log/miner_log_check.txt ) |sendmail -v iamsachinrajput@gmail.com
 
 #(echo "SUBJECT: mail2 Miner will be stopped in $HOSTPUBIP due to low speed = $HRATE" ; cat /skminer/log/miner_log_bad.txt /skminer/log/miner_log_good.txt /skminer/log/miner_log_check.txt ) |sendmail iamsachinrajput@gmail.com
 
