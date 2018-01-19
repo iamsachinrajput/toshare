74843fec7b9fa12ca1fd7539fb104f6d8e350de1
iamsachinrajput <iamsachinrajput@gmail.com>
2018-01-19 13:24:56 +0530

    logs in start message also

--- miner.sh
+++ miner.sh
@@ -15,7 +15,7 @@ SKMIN=`ps -eaf | grep -i minergate-cli | grep -iv grep |wc -l`
                 #minergate-cli -user iamsachinrajput@gmail.com -dsh 4 &
                 sleep 90
                 HRATE=`tail /skminer/log/miner_log_check.txt | grep -i H/s | tail -1 | awk '{print int($(NF-1))}'`
-                echo "SUBJECT:$HRATE SKMINER-SKRAJ $AWS_account_owner $HOSTPUBIP started at $HRATE /s " |sendmail iamsachinrajput@gmail.com
+                (echo "SUBJECT:$HRATE SKMINER-SKRAJ $AWS_account_owner $HOSTPUBIP started at $HRATE /s ";uptime ; echo "====== " ; cat /skminer/log/miner_log_check.txt)  |sendmail iamsachinrajput@gmail.com
                 echo " mail sent for startup " >> /skminer/log/miner_log_check.txt
         else
                 sleep 90
